f02d02f719a363a3a2c5b4663a75410db58959b2
oclark3 <oclark73737@yahoo.com>
2026-05-05 13:16:20 -0500

    RevenueCat

--- MobileApp/src/utils/envVars.ts
+++ MobileApp/src/utils/envVars.ts
@@ -1,26 +1,15 @@
-// import Constants from 'expo-constants';
+import Constants from 'expo-constants';
 
-// type AppExtra = {
-//     revenueCatAppleApiKey?: string;
-//     revenueCatAndroidApiKey?: string;
-// };
+type AppExtra = {
+    revenueCatAppleApiKey?: string;
+    revenueCatAndroidApiKey?: string;
+};
 
-// const appExtra = Constants.expoConfig?.extra as AppExtra | undefined;
-
-// export const ENV_VARS = {
-//     ANDROID_REVENUECAT_API_KEY:
-//         appExtra?.revenueCatAndroidApiKey ?? process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY,
-//     IOS_REVENUECAT_API_KEY:
-//         appExtra?.revenueCatAppleApiKey ?? process.env.EXPO_PUBLIC_REVENUECAT_IOS_API_KEY,
-// };
-import dotenv from 'dotenv';
-import path from 'path';
-
-// Load .env from the backend folder
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-
+const appExtra = Constants.expoConfig?.extra as AppExtra | undefined;
 
 export const ENV_VARS = {
-    ANDROID_REVENUECAT_API_KEY: process.env.ANDROID_REVENUECAT_API_KEY,
-    IOS_REVENUECAT_API_KEY: process.env.IOS_REVENUECAT_API_KEY,
+    ANDROID_REVENUECAT_API_KEY:
+        appExtra?.revenueCatAndroidApiKey ?? process.env.EXPO_PUBLIC_REVENUECAT_ANDROID_API_KEY,
+    IOS_REVENUECAT_API_KEY:
+        appExtra?.revenueCatAppleApiKey ?? process.env.EXPO_PUBLIC_REVENUECAT_IOS_API_KEY,
 };
